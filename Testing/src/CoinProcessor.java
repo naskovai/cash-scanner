e1a62a09a7cb11fcf0a36a9d05069df957b0c414
Dian Nikolov <dian.nikolov91@gmail.com>
2014-01-23 12:26:41 +0200

    Added histogram sub-class

--- Testing/src/CoinProcessor.java
+++ Testing/src/CoinProcessor.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Hashtable;
 
 import org.opencv.core.Mat;
 
@@ -6,6 +7,32 @@ import org.opencv.core.Mat;
 public class CoinProcessor {
 	private MR8FilterBank filterBank;
 	private KMeansFinder kMeansFinder;
+	
+	public class Histogram extends Hashtable<Integer, Double> {
+		public Double compare(final Histogram computedHistogram){
+			Double chiSq = 0d;
+			
+			for (int i = 0; i < this.size(); i++){
+				Double h1 = this.get(i);
+				Double h2 = computedHistogram.get(i);
+				chiSq += (h2 - h1) * (h2 - h1) / h1;
+			}
+			
+			return chiSq;
+		}
+		
+		public void normalize(){
+			Double area = 0d;
+			
+			for (int i = 0; i < this.size(); i++){
+				area += this.get(i);
+			}
+			
+			for (int i = 0; i < this.size(); i++){
+				this.put(i, this.get(i) / area);
+			}
+		}
+	}
 	
 	public CoinProcessor() {
 		filterBank = new MR8FilterBank(17);
