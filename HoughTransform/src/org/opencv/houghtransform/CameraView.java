5d1d914f988d7ae6b3f166d43efb997af2d60bef
Dian Nikolov <dian.nikolov91@gmail.com>
2014-01-30 17:26:09 +0200

    Now taking photos at max resolution again

--- HoughTransform/src/org/opencv/houghtransform/CameraView.java
+++ HoughTransform/src/org/opencv/houghtransform/CameraView.java
@@ -13,83 +13,82 @@ import android.util.Log;
 
 public class CameraView extends JavaCameraView implements PictureCallback {
 
-    private static final String TAG = "Sample::Tutorial3View";
-    private CameraCallback returnInterface;
-    
-    public CameraView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        returnInterface = (CameraCallback) context;
-    }
+	private static final String TAG = "Sample::Tutorial3View";
+	private CameraCallback returnInterface;
 
-    public List<String> getEffectList() {
-        return mCamera.getParameters().getSupportedColorEffects();
-    }
+	public CameraView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		returnInterface = (CameraCallback) context;
+	}
 
-    public boolean isEffectSupported() {
-        return (mCamera.getParameters().getColorEffect() != null);
-    }
+	public List<String> getEffectList() {
+		return mCamera.getParameters().getSupportedColorEffects();
+	}
 
-    public String getEffect() {
-        return mCamera.getParameters().getColorEffect();
-    }
+	public boolean isEffectSupported() {
+		return (mCamera.getParameters().getColorEffect() != null);
+	}
 
-    public void setEffect(String effect) {
-        Camera.Parameters params = mCamera.getParameters();
-        params.setColorEffect(effect);
-        mCamera.setParameters(params);
-    }
+	public String getEffect() {
+		return mCamera.getParameters().getColorEffect();
+	}
 
-    public List<Size> getResolutionList() {
-        return mCamera.getParameters().getSupportedPreviewSizes();
-    }
+	public void setEffect(String effect) {
+		Camera.Parameters params = mCamera.getParameters();
+		params.setColorEffect(effect);
+		mCamera.setParameters(params);
+	}
 
-    public void setResolution(Size resolution) {
-        disconnectCamera();
-        mMaxHeight = resolution.height;
-        mMaxWidth = resolution.width;
-        connectCamera(getWidth(), getHeight());
-    }
+	public List<Size> getResolutionList() {
+		return mCamera.getParameters().getSupportedPreviewSizes();
+	}
 
-    public Size getResolution() {
-        return mCamera.getParameters().getPreviewSize();
-    }
+	public void setResolution(Size resolution) {
+		disconnectCamera();
+		mMaxHeight = resolution.height;
+		mMaxWidth = resolution.width;
+		connectCamera(getWidth(), getHeight());
+	}
 
-    public void takePicture() {
-        Log.i(TAG, "Taking picture");
-       
-        Camera.Parameters params = mCamera.getParameters();
-        List<Camera.Size> sizes = params.getSupportedPictureSizes();
-        int maxHeight = 10000;
-        int maxWidth = 10000;
-        
-        for (Camera.Size size : sizes){
-        	Log.i(TAG, "size.height: " + size.height);
-        	Log.i(TAG, "size.width: " + size.width);
-        	if (size.height < 720 && size.width < 1280 && size.height > 400 && size.width > 400){
-        		maxHeight = size.height;
-        		maxWidth = size.width;
-        		break;
-        	}
-        }
-        params.setPictureSize(maxWidth, maxHeight);
-        mCamera.setParameters(params);
-        // Postview and jpeg are sent in the same buffers if the queue is not empty when performing a capture.
-        // Clear up buffers to avoid mCamera.takePicture to be stuck because of a memory issue
-        mCamera.setPreviewCallback(null);
+	public Size getResolution() {
+		return mCamera.getParameters().getPreviewSize();
+	}
 
-        // PictureCallback is implemented by the current class
-        mCamera.takePicture(null, null, this);
-    }
+	public void takePicture() {
+		Log.i(TAG, "Taking picture");
 
-    @Override
-    public void onPictureTaken(byte[] data, Camera camera) {
-        Log.i(TAG, "Saving a bitmap to file. Size: " + data.length);
-        // The camera preview was automatically stopped. Start it again.
-        //mCamera.startPreview();
-        //mCamera.setPreviewCallback(this);
+		Camera.Parameters params = mCamera.getParameters();
+		List<Camera.Size> sizes = params.getSupportedPictureSizes();
+		int maxHeight = 0;
+		int maxWidth = 0;
 
-        returnInterface.onPictureTaken(data);
+		for (Camera.Size size : sizes) {
+			if (size.height > maxHeight) {
+				maxHeight = size.height;
+				maxWidth = size.width;
+			}
+		}
+		params.setPictureSize(maxWidth, maxHeight);
+		mCamera.setParameters(params);
+		
+		// Postview and jpeg are sent in the same buffers if the queue is not
+		// empty when performing a capture.
+		// Clear up buffers to avoid mCamera.takePicture to be stuck because of
+		// a memory issue
+		mCamera.setPreviewCallback(null);
 
+		// PictureCallback is implemented by the current class
+		mCamera.takePicture(null, null, this);
+	}
 
-    }
+	@Override
+	public void onPictureTaken(byte[] data, Camera camera) {
+		Log.i(TAG, "Saving a bitmap to file. Size: " + data.length);
+		// The camera preview was automatically stopped. Start it again.
+		// mCamera.startPreview();
+		// mCamera.setPreviewCallback(this);
+
+		returnInterface.onPictureTaken(data);
+
+	}
 }
